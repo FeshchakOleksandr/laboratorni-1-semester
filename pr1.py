8112ca6a22107e1d4203e4b54fa50463b56ab5d3
FeshchakOleksandr <f1lessmalow@gmail.com>
2026-09-10 13:25:23 +0300

    Add files via upload

---
+++ pr1.py
@@ -0,0 +1,33 @@
+print("Hello, World!")
+
+
+a = 10                  # int ціле число
+b = 3.5                 # float дробове число
+c = "Привіт"             # str текст
+d = True                # bool так або ні
+e = [1, 2, 3]           # list список
+f = (1, 2, 3)           # tuple кортеж
+g = {"вік": 25}         # dict словник
+h = {1, 2, 3}           # set як список но не можуть повторюватись
+
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
+print(d, type(d))
+print(e, type(e))
+print(f, type(f))
+print(g, type(g))
+print(h, type(h))
+
+
+x = 10
+y = 3
+
+print(x + y)   # додавання
+print(x - y)   # віднімання
+print(x * y)   # множення
+print(x / y)   # ділення
+print(x % y)   # остача від ділення
+print(x // y)  # ціле ділення
+print(x ** y)  # степінь
+print(x == y)  # порівняння (рівне чи ні)
